8c9f371143bffbc1d72eea522270adb5d51a37f8
rk9334183132-bit <rk9334183132@gmail.com>
2026-05-04 00:17:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://study-backend-7rox.onrender.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     const user = {
